e19d028c2d29bc87df1b64aa86069c0d94119e76
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-02 20:49:29 +0600

    update

---
+++ TestCode/Template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// shortcuts
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+
+// type aliases
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+// constants
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+//solve function
+void solve() {
+   vector<int> v={5,21,6,7,1};
+   v.pb(2);
+   sort(all(v));
+}
+
+int main() {
+    fast_io;
+    int t = 1;
+    //cin >> t; 
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
