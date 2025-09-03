fa437be3e31d4e67a254277c894f1ffec00aeb71
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-03 23:21:13 +0600

    update

--- Leetcode/5.cpp
+++ Leetcode/5.cpp
@@ -20,16 +20,44 @@ int isPalindrom(string s) {
     }
     return 1; // palindrome
 }
-
-string solver(string s){
-    if(isPalindrom(s)){
-        return s;
+int solver(string s){
+    int i=1,j; int maxlen=1;
+    int len=1;
+    
+    while(i<s.size()-1){
+         j=1;
+      while(s[i-j]==s[i+j]){
+        len=len+2;
+        j++;
+        //cout<<"palin checking"<<endl;
+      }
+      maxlen=max(maxlen,len);
+      len=1;
+      i++;
+      //   cout<<i<<endl;
     }
-    string left= solver(s.substr(0,s.size()-1));
-    string right= solver(s.substr(1));
-    return left.size() > right.size() ? left : right;
+    //for even size
+    i=1,j=2;
+    int k=1; 
+    len=0;
+    int maxlen2=2;
+    while(i<s.size()-2){ //abba
+
+       while(s[i-k]==s[j+k] && i>=0 && j< s.size()-1 &&s[i]==s[j]){
+        cout<<s[i-k]<<" "<<s[j+k]<<endl;
+             len=len+2;
+             k++;
+       }
+       k=1;
+       maxlen2=max(maxlen2,len);
+       i++;
+       j++;
+    }
+    cout<<"maxlen-1 "<<maxlen<<endl;
+    cout<<"maxlen-2 "<<maxlen2<<endl;
+    return 0;
 }
 int main(){
-   string s="babad";
+   string s="asdbbdsasdsac";
    cout<<solver(s);
 }
