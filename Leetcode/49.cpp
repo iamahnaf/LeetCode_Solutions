7a67c8f245325aa40fcba452fb12173af8c067f2
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-21 21:12:07 +0600

    .

--- Leetcode/49.cpp
+++ Leetcode/49.cpp
@@ -1,7 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        
+                                                                  //Input: strs = ["eat","tea","tan","ate","nat","bat"]
+                                                                 //Output: [["bat"],["nat","tan"],["ate","eat","tea"]]    
+
+                                                                 
 }
 int main(){
 
