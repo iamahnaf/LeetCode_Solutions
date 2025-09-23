004740445ad53452bce74fa67e3993021a657b98
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-23 20:23:07 +0600

    .

--- Leetcode/90.cpp
+++ Leetcode/90.cpp
@@ -4,12 +4,12 @@ void helper(vector<int> &arr, vector<vector<int>> &ans, int idx,vector<int> &tem
     if(idx==0){
         ans.push_back(temp);
     }
-    helper(arr,ans,idx-1,temp.)
-
+    helper(arr,ans,idx-1,temp);
 }
 vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+    vector<int> temp;
         vector<vector<int>> ans;
-        helper(nums,ans);
+        helper(nums,ans,0,temp);
         return ans;
 }
 int main(){
