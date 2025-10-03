8530d520dc2e36fc782d21b61075348d1051e565
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-10-03 22:57:56 +0600

    .

--- Leetcode/347.cpp
+++ Leetcode/347.cpp
@@ -1,9 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solver(vector<int> v,int k){
-      for(auto it: maps){
-        cout<<it.first<<" "<<it.second<<endl;
-      }
+     unordered_map<int,int> maps;
+     
 }
 int main(){
    vector<int> v={4,1,1,1,2,2,3};
