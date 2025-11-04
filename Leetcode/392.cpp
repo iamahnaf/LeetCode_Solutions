d079bb8950aca2f6fecd3e603bf959a03bf7febf
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-11-04 18:38:18 +0600

    .

--- Leetcode/392.cpp
+++ Leetcode/392.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        
         int idx = 0; 
         for (int i = 0; i < s.size(); i++) {
             bool found = false;
