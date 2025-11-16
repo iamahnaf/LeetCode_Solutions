df30cccc45e37a40cb5bb3f4649f9085e36b555a
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-11-16 21:02:52 +0600

    update

--- Leetcode/228.cpp
+++ Leetcode/228.cpp
@@ -1,9 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solver(vector<int>& nums){
-    
+vector<string> solver(vector<int>& nums){
+    vector<set<int>> vset;
+   for(int i=0;i<nums.size();i++){
+     set<int> temp;
+     temp.insert(nums[i]);
+     while(i+1 <nums.size() && nums[i]+1 == nums[i+1]){
+         temp.insert(nums[i+1]);
+         i++;
+     }
+     vset.push_back(temp);
+   }
+   vector<string> ans;
+   for(auto it:vset){
+    if (*it.begin() == *it.rbegin()) {
+    ans.push_back(to_string(*it.begin()));
+      } else {
+    ans.push_back(to_string(*it.begin()) + "->" + to_string(*it.rbegin()));
+     }
+   }
+   return ans;
 }
 int main(){
- vector<int> v={0,2,3,4,6,8,9};
-  solver(v);
+ vector<int> v={-1};
+  vector<string> ans = solver(v);
+  for(auto it:ans){
+    cout<<it<<endl;
+  }
 }
