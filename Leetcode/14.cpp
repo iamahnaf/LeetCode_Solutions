1eab5b40fdab9a7733f16cbb283b78fe7101eba4
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-11-17 23:12:38 +0600

    .

--- Leetcode/14.cpp
+++ Leetcode/14.cpp
@@ -1,7 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std; //prefix are start from zero index
 void solver(vector<string> arr){
-    
+    string check= arr[0];
+    int final_len=0;
+    for(int i=1;i<arr.size();i++){
+      string temp=arr[i]; 
+      int len=0;
+      int j=0,k=0;
+      while( j < temp.size() && k < check.size()){
+        if(check[k]==temp[j]){
+         len++;
+         j++;
+         k++;
+        }else {
+          final_len=min(final_len,len);
+        }
+      }
+    }
 }
 int main(){
    
