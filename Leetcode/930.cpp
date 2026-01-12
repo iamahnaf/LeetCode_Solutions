e0f07864bba42a4c3dabc5ea804fb6ea390e42d9
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2026-01-12 22:59:56 +0600

    .

--- Leetcode/930.cpp
+++ Leetcode/930.cpp
@@ -3,7 +3,6 @@ using namespace std;
 int binary(vector<int> v, int k){
     if(k < 0 ) return 0;
     int l=0,r=0,sum=0,count=0,n=v.size();
-
     while(r<n){   //1,0,1,0,1
        sum=sum+v[r];
        while(sum > k){
