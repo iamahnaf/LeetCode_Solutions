627dfb5ad298688e965c2fa9e940ac8ed2cd757a
iamahnaf <ahnaf930119@gmail.com>
2026-05-18 16:37:21 +0600

    71

---
+++ Leetcode/71.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solver(string s){
+    stack<string> st;
+    vector<string> chars;
+    
+
+
+}
+int main(){
+   string s = "/home/user/Documents/../Picture";
+   solver(s);
+}
--- TestCode/test.cpp
+++ TestCode/test.cpp
@@ -1,8 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solver(vector<int> v){
+void solver(string s){
+   vector<string> str;
+   string temp;
+   for (char it: s){
+      if(it=='/'){
+         str.push_back(temp);
+         //cout<<"string added :"<<temp<<endl;
+         temp.clear();
+      }
+      else{
+        temp+=it;
+      }
+   }
+   str.push_back(temp);
+   //adding in stack
+   vector<string> stk;
+   for(string it: str){
+      if(it == ".."){
+         if(!stk.empty()){
+            stk.pop_back();
+         }
+      }
+      else if(it == "." || it == ""){
+         continue;
+      }
+      else {stk.push_back(it);}
+   }
+   string ans="/";
+   for(string it: stk){
+      ans+=it;
+      ans+="/";
+   }
+   ans.pop_back();
+   cout<<ans;
 }
 int main(){
-   vector<int> x={1,3,5};
-   cout<<x.size()-1;
+   string s="/.../a/../b/c/../d/./";
+   solver(s);
 }
