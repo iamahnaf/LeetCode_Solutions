46b135f56b7baaf93baf153462eb88438f3a8306
iamahnaf <ahnaf930119@gmail.com>
2026-05-18 16:37:35 +0600

    71

--- Leetcode/71.cpp
+++ Leetcode/71.cpp
@@ -1,13 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solver(string s){
-    stack<string> st;
-    vector<string> chars;
-    
-
-
+   vector<string> str;
+   string temp;
+   for (char it: s){
+      if(it=='/'){
+         str.push_back(temp);
+         //cout<<"string added :"<<temp<<endl;
+         temp.clear();
+      }
+      else{
+        temp+=it;
+      }
+   }
+   str.push_back(temp);
+   //adding in stack
+   vector<string> stk;
+   for(string it: str){
+      if(it == ".."){
+         if(!stk.empty()){
+            stk.pop_back();
+         }
+      }
+      else if(it == "." || it == ""){
+         continue;
+      }
+      else {stk.push_back(it);}
+   }
+   string ans="/";
+   for(string it: stk){
+      ans+=it;
+      ans+="/";
+   }
+   ans.pop_back();
+   cout<<ans;
 }
 int main(){
-   string s = "/home/user/Documents/../Picture";
+   string s="/.../a/../b/c/../d/./";
    solver(s);
 }
